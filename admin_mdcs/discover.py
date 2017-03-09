6c2164f1d8fb7509464ba45a79df7a9dbba3198e
Zachary Trautt <zachary.trautt@nist.gov>
2017-03-09 14:26:18 -0500

    changes to permissions of default or anonymous should persist after restart

--- admin_mdcs/discover.py
+++ admin_mdcs/discover.py
@@ -36,7 +36,7 @@ def init_rules():
         #### Get or Create the Group anonymous ####
         ###########################################
         anonymousGroup, created = Group.objects.get_or_create(name=anonymous_group)
-        if not created:
+        if created:
             anonymousGroup.permissions.clear()
 
         ###########################################
@@ -47,45 +47,45 @@ def init_rules():
         ##### Get or Create the default group #####
         ###########################################
         defaultGroup, created = Group.objects.get_or_create(name=default_group)
-        if not created:
+        if created:
             defaultGroup.permissions.clear()
 
-        #### EXPLORE ####
-        explore_access_perm = Permission.objects.get(codename=explore_access)
-        explore_save_query_perm = Permission.objects.get(codename=explore_save_query)
-        explore_delete_query_perm = Permission.objects.get(codename=explore_delete_query)
-        defaultGroup.permissions.add(explore_access_perm)
-        defaultGroup.permissions.add(explore_save_query_perm)
-        defaultGroup.permissions.add(explore_delete_query_perm)
-        #### END EXPLORE ####
-
-        #### CURATE ####
-        curate_access_perm = Permission.objects.get(codename=curate_access)
-        curate_view_data_save_repo_perm = Permission.objects.get(codename=curate_view_data_save_repo)
-        curate_edit_document_perm = Permission.objects.get(codename=curate_edit_document)
-        curate_delete_document_perm = Permission.objects.get(codename=curate_delete_document)
-        defaultGroup.permissions.add(curate_access_perm)
-        defaultGroup.permissions.add(curate_view_data_save_repo_perm)
-        defaultGroup.permissions.add(curate_edit_document_perm)
-        defaultGroup.permissions.add(curate_delete_document_perm)
-        #### END CURATE ####
-
-        #### COMPOSE ####
-        compose_access_perm = Permission.objects.get(codename=compose_access)
-        compose_save_template_perm = Permission.objects.get(codename=compose_save_template)
-        compose_save_type_perm = Permission.objects.get(codename=compose_save_type)
-        defaultGroup.permissions.add(compose_access_perm)
-        defaultGroup.permissions.add(compose_save_template_perm)
-        defaultGroup.permissions.add(compose_save_type_perm)
-        #### END COMPOSE ####
-
-        #### API ####
-        api_access_perm = Permission.objects.get(codename=api_access)
-        defaultGroup.permissions.add(api_access_perm)
-        #### END API ####
-        ###########################################
-        ##### END Get or Create the default group #
-        ###########################################
+            #### EXPLORE ####
+            explore_access_perm = Permission.objects.get(codename=explore_access)
+            explore_save_query_perm = Permission.objects.get(codename=explore_save_query)
+            explore_delete_query_perm = Permission.objects.get(codename=explore_delete_query)
+            defaultGroup.permissions.add(explore_access_perm)
+            defaultGroup.permissions.add(explore_save_query_perm)
+            defaultGroup.permissions.add(explore_delete_query_perm)
+            #### END EXPLORE ####
+    
+            #### CURATE ####
+            curate_access_perm = Permission.objects.get(codename=curate_access)
+            curate_view_data_save_repo_perm = Permission.objects.get(codename=curate_view_data_save_repo)
+            curate_edit_document_perm = Permission.objects.get(codename=curate_edit_document)
+            curate_delete_document_perm = Permission.objects.get(codename=curate_delete_document)
+            defaultGroup.permissions.add(curate_access_perm)
+            defaultGroup.permissions.add(curate_view_data_save_repo_perm)
+            defaultGroup.permissions.add(curate_edit_document_perm)
+            defaultGroup.permissions.add(curate_delete_document_perm)
+            #### END CURATE ####
+    
+            #### COMPOSE ####
+            compose_access_perm = Permission.objects.get(codename=compose_access)
+            compose_save_template_perm = Permission.objects.get(codename=compose_save_template)
+            compose_save_type_perm = Permission.objects.get(codename=compose_save_type)
+            defaultGroup.permissions.add(compose_access_perm)
+            defaultGroup.permissions.add(compose_save_template_perm)
+            defaultGroup.permissions.add(compose_save_type_perm)
+            #### END COMPOSE ####
+    
+            #### API ####
+            api_access_perm = Permission.objects.get(codename=api_access)
+            defaultGroup.permissions.add(api_access_perm)
+            #### END API ####
+            ###########################################
+            ##### END Get or Create the default group #
+            ###########################################
 
     except Exception, e:
         print('ERROR : Impossible to init the rules : ' + e.message)
